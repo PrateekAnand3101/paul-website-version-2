a6546e201492dc771084080bdc727db5b75917d4
Prateek Anand <97834602+PrateekAnand3101@users.noreply.github.com>
2024-12-11 11:34:09 +0530

    new modifications

--- ec/settings.py
+++ ec/settings.py
@@ -113,13 +113,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles')]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles','static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
